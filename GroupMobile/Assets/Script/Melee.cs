a6fdf77278a10f0f4da76e2d7fca97b03bff84ad
John Kaser <20.john.kaser@ksd.org>
2019-03-07 13:23:38 -0800

    screensize

--- GroupMobile/Assets/Script/Melee.cs
+++ GroupMobile/Assets/Script/Melee.cs
@@ -17,25 +17,7 @@ public class Melee: MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
-        
-
-        /*if (Input.GetButton("Fire1") && timer > fireSpeed)
-        {
-            timer = 0;
-            
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            mousePosition.z = 0;
-            Vector3 hitDir = mousePosition - transform.position;
-            hitDir.Normalize();
-            //shootDir *= bulletSpeed;
-            //destination - start pos.
-            GameObject MeleeHitbox = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
-            MeleeHitbox.GetComponent<Rigidbody2D>().velocity = hitDir.normalized * sliceSpeed;
-            Destroy(MeleeHitbox, HitTime);
-           // Camera.main.GetComponent<AudioSource>().PlayOneShot(soundToPlay);
-
-        }*/
+       
     }
     public void Attack()
     {
